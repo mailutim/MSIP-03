456a55c5e36a154016248ab71e0cf546054f6c42
Tim254 <tim.mailu@gmail.com>
2022-05-14 23:23:24 +0300

    create array of questions for the quiz

---
+++ assets/js/quiz.js
@@ -0,0 +1,38 @@
+const quizzledata [
+
+   {
+        question: "Which command should you use to display the current path of your current directory?"
+        a: "pwd"
+        b: "cd"
+        c: "ls"
+        d: "touch" 
+        correct: "a"  
+   }
+   {
+       question: "Which command should you use to list all files of your current directory?"
+        a: "pwd"
+        b: "cd"
+        c: "ls"
+        d: "touch" 
+        correct: "c"
+   }
+   {
+       question: "Which command should you use to display the content of a file?"
+       a:"pwd"
+       b: "less"
+       c: "cd"
+       d: "ls"
+       correct: "b"
+   }
+   {
+       question: "Which command should you use to delete a directory?"
+       a: "cp"
+       b: "mv"
+       c: "rm"
+       d: "rmdir"
+       correct: "d"
+   }
+   
+       
+   
+]
