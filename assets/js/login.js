5b0ab94e629b5a97da81bd4cabd4aacb3a3c8fc7
Tim254 <tim.mailu@gmail.com>
2022-05-15 19:26:31 +0300

    create a js file for the login page

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+function store(){
+    let name = document.getElementById("email");
+    var passwad = document.getElementById("psw");
+
+    if (name.value.length == 0) {
+        alert('Please fill in your email address')
+    }
+}
